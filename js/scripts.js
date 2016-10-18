2910a0e0c9c81cbb8bc7578eea947e48d7bb3407
Ryan Mathisen and Tracie Weitzman <ryan@>
2016-10-18 14:46:12 -0700

    fix scripts.js function and add (this)

--- js/scripts.js
+++ js/scripts.js
@@ -1,113 +1,118 @@
+// $(document).ready(function() {
+//   $("#click1").click(function() {
+//     $("#list1 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click2").click(function() {
+//     $("#list2 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click3").click(function() {
+//     $("#list3 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click4").click(function() {
+//     $("#list4 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click5").click(function() {
+//     $("#list5 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click6").click(function() {
+//     $("#list6 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click7").click(function() {
+//     $("#list7 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click8").click(function() {
+//     $("#list8 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click9").click(function() {
+//     $("#list9 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click10").click(function() {
+//     $("#list10 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click11").click(function() {
+//     $("#list11 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click12").click(function() {
+//     $("#list12 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click13").click(function() {
+//     $("#list13 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click14").click(function() {
+//     $("#list14 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click15").click(function() {
+//     $("#list15 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click16").click(function() {
+//     $("#list16 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click17").click(function() {
+//     $("#list17 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click18").click(function() {
+//     $("#list18 > .myDef").toggleClass("showDef");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $("#click19").click(function() {
+//     $("#list19 > .myDef").toggleClass("showDef");
+//   });
+// });
 $(document).ready(function() {
-  $("#click1").click(function() {
-    $("#list1 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click2").click(function() {
-    $("#list2 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click3").click(function() {
-    $("#list3 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click4").click(function() {
-    $("#list4 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click5").click(function() {
-    $("#list5 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click6").click(function() {
-    $("#list6 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click7").click(function() {
-    $("#list7 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click8").click(function() {
-    $("#list8 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click9").click(function() {
-    $("#list9 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click10").click(function() {
-    $("#list10 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click11").click(function() {
-    $("#list11 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click12").click(function() {
-    $("#list12 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click13").click(function() {
-    $("#list13 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click14").click(function() {
-    $("#list14 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click15").click(function() {
-    $("#list15 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click16").click(function() {
-    $("#list16 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click17").click(function() {
-    $("#list17 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click18").click(function() {
-    $("#list18 > .myDef").toggleClass("showDef");
-  });
-});
-
-$(document).ready(function() {
-  $("#click19").click(function() {
-    $("#list19 > .myDef").toggleClass("showDef");
+  $(".well").click(function() {
+    $(this).children().toggleClass("showDef");
   });
 });
